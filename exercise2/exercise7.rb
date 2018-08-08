7e6ed7840e2d4e40b675652532bddbc2a2874fbf
marmo002 <marmo_02@hotmail.com>
2018-08-08 12:57:52 -0400

    finished exercise 7

---
+++ exercise2/exercise7.rb
@@ -0,0 +1,26 @@
+
+def wrap_text(text, symbol)
+  return symbol + text + symbol
+end
+
+# puts wrap_text("martin", "***")
+# puts wrap_text("martin", '---===###')
+
+def better_wrap(text, symbol)
+
+  s_count = symbol.length
+
+  first_text = wrap_text(text, symbol).chomp(symbol)
+
+  new_symb = ""
+
+  s_count.times do |x|
+    new_symb += symbol[s_count - 1]
+    s_count -= 1
+  end
+
+  first_text += new_symb
+
+end
+
+puts better_wrap("word", "++===")
